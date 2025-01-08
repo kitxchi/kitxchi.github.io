97ee5ec54e5e9e7408965b97aa20e03acc498e25
kitxchi <kitxchi@gmail.com>
2025-01-08 10:44:37 +0100

    Add files via upload

---
+++ gg/else.php
@@ -0,0 +1,35 @@
+<h1 style="color:#37915d">Franek GG site</h1>
+<br>
+<?php
+if ($a > $b) {
+  echo "a is greater than b";
+} else {
+  echo "a is NOT greater than b";
+}
+?>
+<style>
+          @font-face {
+               font-family: matrix;
+               src: url(font/kropki.ttf);
+          }
+          html{
+               text-align:center;
+               background-color:black;
+               color:#c0fad8;
+               font-family: "matrix", sans-serif;
+               margin-top: 30px;
+               font-size: 20px;
+          }
+          h1{
+            font-size: 54px;
+          }
+          h3{
+            color:#37915d;
+            font-size: 30px;
+            }
+          a{
+            color: #c0fad8;
+            text-decoration: none;
+          }
+          </style>
+<a href="https://github.com/kitxchi/kitxchi.github.io/blob/main/gg/else.php">Zobacz kod strony</a>
---
+++ gg/if.php
@@ -0,0 +1,34 @@
+<h1 style="color:#37915d">Franek GG site</h1>
+<br>
+<?php
+if ($a > $b) {
+  echo "a is bigger than b";
+  $b = $a;
+}
+?>
+        <style>
+          @font-face {
+               font-family: matrix;
+               src: url(font/kropki.ttf);
+          }
+          html{
+               text-align:center;
+               background-color:black;
+               color:#c0fad8;
+               font-family: "matrix", sans-serif;
+               margin-top: 30px;
+               font-size: 20px;
+          }
+          h1{
+            font-size: 54px;
+          }
+          h3{
+            color:#37915d;
+            font-size: 30px;
+            }
+          a{
+            color: #c0fad8;
+            text-decoration: none;
+          }
+          </style>
+<a href="https://github.com/kitxchi/kitxchi.github.io/blob/main/gg/if.php">Zobacz kod strony</a>
